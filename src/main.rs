b0d7960b78e24e96ca45bbebedadd839cbf3c943
Floozutter <me@floozutter.site>
2025-05-22 00:12:16 -0700

    use sqlx

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
+use sqlx::migrate::MigrateDatabase;
 use std::path::PathBuf;
 
 const DATABASE_FILENAME: &str = "pearlden.db";
 
-fn main() {
+#[tokio::main]
+async fn main() {
     // parse command-line arguments
     let matches = clap::command!()
         .arg(
             clap::Arg::new("den")
                 .required(true)
                 .value_parser(clap::value_parser!(PathBuf))
+                .help("Path to directory of den")
+        )
+        .arg(
+            clap::Arg::new("create")
+                .short('c')
+                .long("create")
+                .action(clap::ArgAction::SetTrue)
+                .help("Create database file if absent")
         )
         .get_matches();
     let den_path = matches.get_one::<PathBuf>("den").expect("<den> should be required");
@@ -19,25 +29,27 @@ fn main() {
         std::process::exit(1);
     }
     let db_path = den_path.join(DATABASE_FILENAME);
-    println!("verifying \"{}\" is a file...", db_path.display());
-    if !db_path.is_file() {
-        eprintln!("error: \"{}\" is not a file!", db_path.display());
-        std::process::exit(1);
+    let db_url = format!("sqlite://{}", db_path.display());
+    println!("verifying \"{}\" exists...", db_path.display());
+    if !sqlx::Sqlite::database_exists(&db_url).await.unwrap_or(false) {
+        if !matches.get_flag("create") {
+            eprintln!("error: \"{}\" does not exist!", db_path.display());
+            std::process::exit(1);
+        }
+        println!("creating \"{}\"...", db_path.display());
+        if let Err(error) = sqlx::Sqlite::create_database(&db_url).await {
+            eprintln!("error: could not create \"{}\" ({})!", db_path.display(), error);
+            std::process::exit(1);
+        }
     }
     // start server
     println!("starting server...");
     let app = axum::Router::new().route("/", axum::routing::get(handler));
-    tokio::runtime::Builder::new_current_thread()
-        .enable_all()
-        .build()
-        .unwrap()
-        .block_on(async {
-            let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
-                .await
-                .unwrap();
-            println!("listening on: {}", listener.local_addr().unwrap());
-            axum::serve(listener, app).await.unwrap();
-        });
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    println!("listening on: {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
 }
 
 async fn handler() -> axum::response::Html<&'static str> {
